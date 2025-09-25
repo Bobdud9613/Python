387e336d00fedbf26c6c1ef3df3792e5bf56055f
Bobdud9613 <agala561@evit.edu>
2025-09-25 12:40:37 -0700

    Update RNGtest.py

--- RNGtest.py
+++ RNGtest.py
@@ -6,19 +6,18 @@ import random
 roll = random.randint(1, 10000)
 user = input("Press enter to roll. Type \'h\' for a list of commands ")
 
-while user != "b":
+while True:
     user = user.lower()
-    if user == "h":
-        print()
-        print("\'r\' for results\n\'b\' to exit")
-    if user == "r":
-        print()
-        print("Results: ")
-    if user == "b":
-        break
 
-    print()
-    print(str(float(roll / 100)))
-    print()
+    match user:
+        case "h":
+            print("\n\'r\' for results\n\'b\' to exit\n")        
+        case "r":
+            print("\nResults: \n")
+        case "b":
+            break
+        case "":
+            print("\n" + str(float(roll / 100)) + "\n")
+
     user = input("Press enter to roll. Type \'h\' for a list of commands ")
     roll = random.randint(1, 10000)
